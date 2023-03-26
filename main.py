ce1ef7d885300eff8fe821b1ef743d05caa6bd15
Adil <adil.bouamrioune@gmail.com>
2023-03-26 22:59:09 +0200

    simple responsive div for the futur java code and result

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ def hello():
 root = tk.Tk()
 
 root.title("Java 319")
-root.geometry("400x300")
+root.geometry("1000x800")
 root.configure(bg="#01192E")
+
+# Création de la div principale avec une couleur de fond
+main_div = tk.Frame(root, width=400, height=300, bg="#353535")
+main_div.pack(fill="both", expand=True, padx=40, pady=40)
 
 # menu
 menu1 = tk.Menu(root)
@@ -15,3 +19,8 @@ menu1.add_command(label="app 1", command=hello)
 root.config(menu=menu1)
 
 root.mainloop()
+
+
+
+
+
