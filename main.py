9d7ff7ffbaf14f837d8ac8c4314b37aff1029107
Adil <adil.bouamrioune@gmail.com>
2023-03-26 22:33:14 +0200

    menu tkinter

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ root = tk.Tk()
 
 root.title("Java 319")
 root.geometry("400x300")
+root.configure(bg="#01192E")
+
+# menu
+menu1 = tk.Menu(root)
+menu1.add_cascade(label="app 1")
+root.config(menu=menu1)
 
 root.mainloop()
+
