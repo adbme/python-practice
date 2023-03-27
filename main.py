f67ff4414f74050b43a545092e7e1957db262f9f
Adil <adil.bouamrioune@gmail.com>
2023-03-27 22:35:14 +0200

    Adding the content of a print to the input with execute button

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from tkinter.scrolledtext import ScrolledText
 
 root = tk.Tk()
 
-def hello():
-    print("hello")
-
 # style
 root.title("Java 319")
 root.geometry("1000x800")
 root.configure(bg="#2A2A2A")
+
+
 
 # Fonction execute
 def execute_code():
@@ -20,9 +19,20 @@ def execute_code():
         result = str(e)  # trouve l'erreur
     console.insert(tk.END, result + "\n")  # systeme erreure
 
+
+
+# Fonction lien 1
+def ex1():
+    input1.delete("1.0", tk.END)
+    input1.insert(tk.END, 'print("exercie 1")')
+
+
+
 # div
 main_div = tk.Frame(root, width=100, height=300, bg="#2A2A2A")
 main_div.pack(fill="both", expand=True, padx=20, pady=20)
+
+
 
 # label input
 label1 = tk.Label(main_div, text="CODE:", font=("Arial", 20), fg="#FFFFFF", bg="#2A2A2A")
@@ -31,11 +41,15 @@ input1 = tk.Text(main_div, width=50, height=10, font=("Arial", 20), bd=2, relief
 input1.bind("<Shift-Return>", lambda event: input1.insert(tk.END, '\n')) # retour a la ligne
 input1.pack(fill="x")
 
+
+
 # label console
 label2 = tk.Label(main_div, text="CONSOLE", font=("Arial", 20), fg="#FFFFFF", bg="#2A2A2A")
 label2.pack(pady=(0, 10))
 console = ScrolledText(main_div, width=50, height=0, font=("Arial", 20), bd=2, relief="groove", fg="#FFFFFF", bg="#3D3D3D")
 console.pack(fill="both", expand=True)
+
+
 
 # boutton execute
 execute_button = tk.Button(main_div, text="Execute", font=("Arial", 20), bd=2, relief="groove", command=execute_code, fg="#FFFFFF", bg="#3D3D3D")
@@ -43,7 +57,7 @@ execute_button.pack(pady=(20,0))
 
 # menu
 menu1 = tk.Menu(root, fg="#FFFFFF", bg="#2A2A2A")
-menu1.add_command(label="app 1", command=hello)
+menu1.add_command(label="ex 1", command=ex1)
 root.config(menu=menu1)
 
 x = ('test')
